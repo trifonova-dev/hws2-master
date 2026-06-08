00aac46dc09551d986548fab9bd3e775d6183259
Natalia Trifonova <trifonova.dev@gmail.com>
2026-06-08 12:48:51 +0700

    fix-3_1

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,20 +1,36 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
-import { UserType } from './HW3'
+import {UserType} from './HW3'
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[] // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
 export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
+    if (!name) {
+        setError(error)
+    } else {
+        addUserCallback(name)
+        setName("")
+    }
+
     // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
 }
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+export const pureOnBlur = (name: any, setError: any) => {
+    if (!name) {
+        setError(error)
+    }// если имя пустое - показать ошибку
 }
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
+export const pureOnEnter = (e: any, addUser: any) => {
+    if (e.key === "Enter") {
+        addUser()
+    }// если нажата кнопка Enter - добавить
 }
 
 // более простой и понятный для новичков
@@ -22,32 +38,30 @@ export const pureOnEnter = (e: any, addUser: any) => { // если нажата 
 
 // более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
-    // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
+    // деструктуризация проп
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
-
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setName(e.currentTarget.value) // need to fix
         error && setError('')
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
     }
-
     const onBlur = () => {
         pureOnBlur(name, setError)
     }
 
-    const onEnter = (e: any) => {
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+    const totalUsers = users.length// need to fix
+    const lastUserName = name// need to fix
 
     return (
         <Greeting
--- src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import {pureAddUserCallback} from '../HW3'
+import {pureAddUser, pureOnBlur, pureOnEnter} from "../GreetingContainer";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 let initialState: any[]
 const setName = (a: any[]) => {
@@ -15,4 +18,64 @@ test('name 1', () => {
     expect(initialState.length).toBe(1)
     expect(initialState[0].name).toBe('name')
     expect(!!initialState[0]._id).toBe(true)
+})
+
+
+test('name 2 - пустое имя показывает ошибку', () => {
+    const setError = jest.fn()
+    const setName = jest.fn()
+    const addUserCallback = jest.fn()
+
+    pureAddUser("", setError, setName, addUserCallback)
+
+    expect(setError).toHaveBeenCalled()
+    expect(addUserCallback).not.toHaveBeenCalled()
+})
+
+test('name 3 - имя есть добавляет юзера', () => {
+    const setError = jest.fn()
+    const setName = jest.fn()
+    const addUserCallback = jest.fn()
+
+    pureAddUser("Иван", setError, setName, addUserCallback)
+
+    expect(addUserCallback).toHaveBeenCalledWith("Иван")
+    expect(setName).toHaveBeenCalledWith("")
+    expect(setError).not.toHaveBeenCalled()
+})
+
+test('name 4 - пустое имя показывает ошибку', () => {
+    const setError = jest.fn()
+
+    pureOnBlur("", setError)
+    expect(setError).toHaveBeenCalled()
+})
+
+test('name 5 - когда имя есть, ', () => {
+    const setError = jest.fn()
+
+    pureOnBlur("", setError)
+    expect(setError).toHaveBeenCalled()
+})
+
+test('name 6 - имя есть, ошибки нет, ', () => {
+    const setError = jest.fn()
+
+    pureOnBlur("Mary", setError)
+    expect(setError).not.toHaveBeenCalled()
+})
+
+
+test('name 6 - если нажали кнопку Enter, ', () => {
+    const addUser = jest.fn()
+
+    pureOnEnter({key: "Enter"}, addUser)
+    expect(addUser).toHaveBeenCalled()
+})
+
+test('name 7 - если нажали НЕ на кнопку Enter, ', () => {
+    const addUser = jest.fn()
+
+    pureOnEnter({key: "a"}, addUser)
+    expect(addUser).not.toHaveBeenCalled()
 })
