65940c8bcd55efbafd4aa5c01e5040f72c895a6f
Natalia Trifonova <trifonova.dev@gmail.com>
2026-08-02 19:06:32 +0700

    hw-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            if (action.payload === "up") {
+                return [...state].sort((a, b) => a.name.localeCompare(b.name))
+            }
+            else {
+                return [...state].sort((a, b) => b.name.localeCompare(a.name))
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(human => human.age >= action.payload) // need to fix
         }
         default:
             return state
