b28a2f5f0d6cb5a450f23e8fced4797c565c346f
Natalia Trifonova <trifonova.dev@gmail.com>
2026-08-28 12:33:51 +0700

    hw-9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import {restoreState} from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
@@ -10,30 +10,52 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
+        const id = setInterval(() => {
+            setDate(new Date())
+        }, 1000);
+
+        return () => clearInterval(id)
+
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
 
     }
 
     const stop = () => {
+        setShow(true)
+        clearInterval(timerId)
+        setTimerId(undefined)
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
 
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
+        setShow(true)
     }
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+        setShow(false)
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = `${date.getHours().toString().padStart(2, "0")}:
+    ${date.getMinutes().toString().padStart(2, "0")}:
+    ${date.getSeconds().toString().padStart(2, "0")} `;
+    // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
 
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    // 12:30:00
 
+    const stringDate = `${date.getFullYear().toString().padStart(2, "0")}:
+    ${date.getMonth().toString().padStart(2, "0")}:
+    ${date.getDate().toString().padStart(2, "0")}`;
+    {/*// день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем*/
+    }
+
+
+    {/*// день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)*/
+    }
+    const stringDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'})
+        .format(date) || <br/> // пишут студенты
+    const stringMonth = new Intl.DateTimeFormat('en-US', {month: 'long'})
+        .format(date) || <br/> // пишут студенты
     return (
         <div className={s.clock}>
             <div
